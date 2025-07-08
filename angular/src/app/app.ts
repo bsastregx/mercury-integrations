cf5f03563f4f4a9bcafd9c19a39a54e167ee0e89
Bruno Sastre <bsastre@genexus.com>
2025-07-08 16:45:02 -0300

    `[angular]` Update angular custom-dialog

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { CustomDialogComponent } from "./custom-dialog/custom-dialog";
 
+import { ThemeModel } from "@genexus/chameleon-controls-library";
+import { bundleToHashMappings } from "../assets/mercury-css/bundle-to-hash-mappings";
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  selector: "app-root",
+  standalone: true,
+  imports: [RouterOutlet, CustomDialogComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
 })
 export class App {
-  protected title = 'angular';
+  protected title = "angular";
+
+  mercuryIconsBundle: ThemeModel = [
+    {
+      name: "mercury/base/icons",
+      url: "/assets/css/" + `${bundleToHashMappings["base/icons"]}.css`,
+    },
+  ];
 }
--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,22 +1,24 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { App } from "./app/app";
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+// Mercury
+import { setBundleMapping } from "@genexus/mercury";
+import { bundleToHashMappings } from "./assets/mercury-css/bundle-to-hash-mappings";
+import { getImagePathCallbackDefinitions } from "@genexus/mercury";
+import { registerMercury } from "@genexus/mercury";
 
-// Mercury / Chameleon
-import { setBundleMapping } from '@genexus/mercury';
-import { bundleToHashMappings } from './assets/mercury-css/bundle-to-hash-mappings';
+// Chameleon
+import { registryProperty } from "@genexus/chameleon-controls-library/dist/collection";
+import { defineCustomElements } from "@genexus/chameleon-controls-library/loader";
+
+registerMercury();
 setBundleMapping(bundleToHashMappings);
-
-import { registryProperty } from '@genexus/chameleon-controls-library/dist/collection';
-import { getImagePathCallbackDefinitions } from '@genexus/mercury';
-import { registerMercury } from '@genexus/mercury';
-
 // Register the default value for the getImagePathCallback property in all Chameleon
 // components. This implementation allows us to remove the need for binding the
 // getImagePathCallback property in all Chameleon controls that must render icons.
-registryProperty('getImagePathCallback', getImagePathCallbackDefinitions);
+registryProperty("getImagePathCallback", getImagePathCallbackDefinitions);
+defineCustomElements(window);
 
 // It registers a mapping between the icons metadata and its path.
-registerMercury();
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
