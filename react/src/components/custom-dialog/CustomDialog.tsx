7532d5f83e83d01489ce93f91afbc9fcc9230291
Bruno Sastre <bsastre@genexus.com>
2025-07-08 18:54:32 -0300

    `[react]` General update

---
+++ react/src/components/custom-dialog/CustomDialog.tsx
@@ -0,0 +1,23 @@
+import { getIconPath } from "@genexus/mercury/assets-manager.js";
+import { getBundles } from "@genexus/mercury/bundles.js";
+import { ChTheme, ChImage } from "../../chameleon-components";
+
+const CSS_THEME = getBundles(
+  ["components/button", "components/icon"],
+  "/assets/css/"
+);
+const ICON = getIconPath({
+  category: "system",
+  name: "add-circle",
+  colorType: "primary",
+});
+
+export const CustomDialog = () => (
+  <>
+    <ChTheme model={CSS_THEME} />
+    <button className="button-primary button-icon-and-text" type="button">
+      <ChImage className="icon-md" src={ICON} type="mask" />
+      Caption
+    </button>
+  </>
+);
